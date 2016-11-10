cc8192c91907c3d43acaf7f5800b9cd14add8bb6
yi0x262 <yutadefn@gmail.com>
2016-11-10 23:24:58 +0900

    add jointnames list

--- genYAML.py
+++ genYAML.py
@@ -4,6 +4,7 @@ class genYAML(object):
     def __init__(self,robotname):
         self.joints = {}
         self.yaml = {robotname:self.joints}
+        self.controllers = []
 
     def output(self):
         return yaml.dump(self.yaml,default_flow_style=False)
@@ -14,6 +15,7 @@ class genYAML(object):
         info.update({'publish_rate':publist_rate})
         jsc = {'joint_state_controller':info}
         self.joints.update(jsc)
+        self.controllers.append('joint_state_controller')
 
     def revolute_joint_controller(self,jointname,pid={'p':1,'i':0,'d':0}):
         #must : use jointtype argument
@@ -23,9 +25,12 @@ class genYAML(object):
         info.update({'pid':pid})#wanna this line flow_style...
         rjc = {jointname+'_position_controller':info}
         self.joints.update(rjc)
+        self.controllers.append(jointname+'_position_controller')
+
 
 if __name__ == '__main__':
     c = genYAML('robot')
     c.joint_state_controller(50)
     c.revolute_joint_controller('test')
     print(c.output())
+    print(c.controllers)
